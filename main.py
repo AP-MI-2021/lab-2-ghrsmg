5aee6c1e3fd27bf5ca580e531423148f2bdb770d
Gherasim Gabriel <gherasimgabriel309@gmail.com>
2021-10-04 16:27:07 +0300

    lab 2 3 probleme

--- main.py
+++ main.py
@@ -20,7 +20,6 @@ def get_n_choose_k(n, k):
 
 get_n_choose_k(10, 6)
 
-
 def test_get_n_choose_k():
     if get_n_choose_k(10, 8) == 45:
         pass
@@ -29,9 +28,7 @@ def test_get_n_choose_k():
     if get_n_choose_k(15, 10) == 3003:
         pass
 
-
 '''problema 11'''
-
 
 def get_leap_years(y1, y2):
     if y1 > y2:
@@ -60,5 +57,28 @@ def test_get_perfect_squares():
     if get_perfect_squares(5, 9) == 3:
         pass
 
-
 get_perfect_squares(70, 150)
+def main() :
+    n=int(input("Dati numarul: "))
+    k = int(input("Dati numarul: "))
+    while k!=-1:
+        print(get_n_choose_k(n,k))
+        n = int(input("Dati alt numarul: "))
+        k = int(input("Dati alt numarul: "))
+def main():
+    y1 = int(input("Dati anul: "))
+    y2= int(input("Dati numarul: "))
+    while y2!=-1:
+        print (get_leap_years(y1,y2))
+        y1 = int(input("Dati alt an: "))
+        y2 = int(input("Dati alt an: "))
+def main():
+    start = int(input("Dati numarul: "))
+    end = int(input("Dati numarul: "))
+    while end != -1:
+        print(get_perfect_squares(start, end))
+        start = int(input("Dati alt numarul: "))
+        end = int(input("Dati alt numarul: "))
+
+
+
