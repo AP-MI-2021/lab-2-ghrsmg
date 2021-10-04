98db06b293a6174ebebde25121c38632cd089c4f
Gherasim Gabriel <gherasimgabriel309@gmail.com>
2021-10-04 08:36:06 +0300

    Problema 10 si 11

--- main.py
+++ main.py
@@ -21,7 +21,18 @@ def test_get_n_choose_k():
         pass
     if get_n_choose_k(15,10)==3003:
         pass
+'''problema 11'''
+def get_leap_years(y1, y2):
+    if y1 > y2:
+        x = y1
+        y1 = y2
+        y2 = x
+    for i in range(y1-1, y2+1, 1):
+        if i % 4 == 0:
+            print(i)
 
+
+get_leap_years(2000, 2100)
 
 
 
