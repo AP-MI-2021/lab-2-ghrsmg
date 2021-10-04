724f417ab2cee34b16d093c5b7c5db9ce9b8dd25
Gherasim Gabriel <gherasimgabriel309@gmail.com>
2021-10-04 08:26:57 +0300

    Problema 10

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-'''problema 11'''
-def get_leap_years(y1, y2):
-    if y1 > y2:
-        x = y1
-        y1 = y2
-        y2 = x
-    for i in range(y1-1, y2+1, 1):
-        if i % 4 == 0:
-            print(i)
+def get_n_choose_k(n,k):
+    nfact=1
+    kfact=1
+    mfact=1
+    m=n-k
+    while(n):
+        nfact=nfact*n
+        n=n-1
+    while(k):
+        kfact=kfact*k
+        k=k-1
+    while(m):
+        mfact=mfact*m
+        m=m-1
+    print (nfact//(kfact*mfact))
+
+def test_get_n_choose_k():
+    if get_n_choose_k(10,8)==45 :
+        pass
+    if get_n_choose_k(12,6)==924:
+        pass
+    if get_n_choose_k(15,10)==3003:
+        pass
 
 
-get_leap_years(2000, 2100)
+
+
